a2a0ede3c175c98959735595c5db9a42240274ee
Waseem Arain <waseemarain4u@gmail.com>
2018-03-20 16:55:29 +0500

    initial create
    Email with CC and BCC

---
+++ app/src/main/java/com/waseemarain/backgroundemail/MainActivity.java
@@ -0,0 +1,49 @@
+package com.waseemarain.backgroundemail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.waseemarain.backgroundemaillibrary.BackgroundMail;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button= findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendTestEmail();
+            }
+        });
+    }
+
+    private void sendTestEmail(){
+        BackgroundMail.newBuilder(this)
+                .withUsername("username@gmail.com")
+                .withPassword("Password12345")
+                .withMailto("to@gmail.com")
+                .withMailCc("cc@gmail.com")
+                .withMailBcc("bcc@gmail.com")
+                .withSubject("this is the subject")
+                .withBody("this is the body")
+                .withOnSuccessCallback(new BackgroundMail.OnSuccessCallback() {
+                    @Override
+                    public void onSuccess() {
+                        //do some magic
+                    }
+                })
+                .withOnFailCallback(new BackgroundMail.OnFailCallback() {
+                    @Override
+                    public void onFail() {
+                        //do some magic
+                    }
+                })
+                .send();
+    }
+}
